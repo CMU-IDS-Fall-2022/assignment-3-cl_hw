b92f4a30037cc0210f0bee3499ddb78e66e51e60
DreamNengNeng <75749274+DreamNengNeng@users.noreply.github.com>
2022-10-13 15:50:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,13 @@ image = Image.open('unleashing-innovation-image.jpg')
 col1, col2, col3 = st.beta_columns([1,6,1])
 
 with col1:
-st.write("")
+    st.write("")
 
 with col2:
-st.image(image, caption ='Innovation')
+    st.image(image, caption ='Innovation')
     
 with col3:
-st.write("")
+    st.write("")
 
 st.image(image, caption ='Innovation')
 
